1aa5aef41b18f5fc655c5fbad84888377528ad93
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-04 02:37:15 +0700

    bt1

---
+++ Bai2KieuDulieu/Baitap1.java
@@ -0,0 +1,17 @@
+package Bai2KieuDulieu;
+
+public class Baitap1 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 3.14;
+        char c = 'A';
+        boolean d = true;
+        String e = "Hello, Java!";
+
+        System.out.println("Giá trị của a: " + a);
+        System.out.println("Giá trị của b: " + b);
+        System.out.println("Giá trị của c: " + c);
+        System.out.println("Giá trị của d: " + d);
+        System.out.println("Giá trị của e: " + e);   
+    }
+}
