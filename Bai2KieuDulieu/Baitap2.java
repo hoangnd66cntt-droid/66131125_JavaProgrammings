6b5b62fe95ae1e4be5c4d642a949e1b0322d73b8
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-04 02:37:25 +0700

    bt2

---
+++ Bai2KieuDulieu/Baitap2.java
@@ -0,0 +1,9 @@
+package Bai2KieuDulieu;
+public class Baitap2 {
+    public static void main(String[] args) {
+        double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+    }
+}
