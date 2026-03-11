0ea2836db5f14a2e1b4cc746783eaf40bc46ebe4
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-11 22:19:02 +0700

    bai3

---
+++ Bai7CONSTRUCTORvatinhdonggoi/diemoxy/Diem.java
@@ -0,0 +1,27 @@
+package Bai7CONSTRUCTORvatinhdonggoi.diemoxy;
+import java.lang.Math;
+public class Diem {
+    private double x;
+    private double y;
+    public Diem() {
+        this.x = 0;
+        this.y = 0;
+    }
+    public Diem(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double tinhKhoangCach(Diem d) {
+        return Math.sqrt(Math.pow(this.x - d.x, 2) +
+                         Math.pow(this.y - d.y, 2));
+    }
+    public boolean kiemTraTrungNhau(Diem d) {
+        return this.x == d.x && this.y == d.y;
+    }
+    public Diem doiXungQuaGoc() {
+        return new Diem(-this.x, -this.y);
+    }
+    public void tostring() {
+        System.out.println("(" + x + ", " + y + ")");
+    }
+}
