c17c88b8b2527ef9cf7bfd1f3793739d97f07147
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-12 01:51:00 +0700

    quen dong goi

--- Bai7CONSTRUCTORvatinhdonggoi/nhanvien/Main.java
+++ Bai7CONSTRUCTORvatinhdonggoi/nhanvien/Main.java
@@ -1,11 +1,15 @@
-package Bai7CONSTRUCTORvatinhdonggoi;
+package Bai7CONSTRUCTORvatinhdonggoi.nhanvien;
 
 public class Main {
     public static void main(String[] args) {
-        Nhanvien nv1 = new Nhanvien("NV001", "Nguyễn Văn A", 10000000, 2.5);
-        Nhanvien nv2 = new Nhanvien("NV002", "Trần Thị B", 8000000, 2.0); 
-        Nhanvien nv3 = new Nhanvien("NV003", "Lê Văn C", 5000000, 2.0);
+        nhanvien nv1 = new nhanvien("NV001", "Nguyễn Văn A", 10000000, 2.5);
+        nhanvien nv2 = new nhanvien("NV002", "Trần Thị B", 8000000, 2.0); 
+        nhanvien nv3 = new nhanvien("NV003", "Lê Văn C", 5000000, 2.0);
+        nhanvien nv4 = new nhanvien("NV004", "Phung Thanh D", 3600000, 2.0);
+
         nv1.hienThiThongTin();
         nv2.hienThiThongTin();
         nv3.hienThiThongTin();
+        nv4.hienThiThongTin();
     }
+}
