691995fe15b4741a415ccf43f2e97a32feeaa2c6
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-19 09:17:36 +0700

    xoa getter setter

--- Bai8TinhKeThua/VD2NhanVien/NhanVienVanPhong.java
+++ Bai8TinhKeThua/VD2NhanVien/NhanVienVanPhong.java
@@ -2,38 +2,21 @@ package Bai8TinhKeThua.VD2NhanVien;
 
 public class NhanVienVanPhong extends NhanVien {
     private int soGioLamViec;
-    private double tienPhuCap;
+    private double luongThemMoiGio;
 
-    public NhanVienVanPhong(String maNV, String hoTen, double luongCoBan, int soGioLamViec, double tienPhuCap, int soNgayLamViec) {
+    public NhanVienVanPhong(String maNV, String hoTen, double luongCoBan, int soGioLamViec, double luongThemMoiGio, int soNgayLamViec) {
         super(maNV, hoTen, luongCoBan);
         this.soGioLamViec = soGioLamViec;
         this.soGioLamViec = soGioLamViec;
-        this.tienPhuCap = tienPhuCap;
+        this.luongThemMoiGio = luongThemMoiGio;
     }
 
     @Override
     public double tinhLuong() {
-        return luongCoBan * soGioLamViec + tienPhuCap;
+        return luongCoBan * soGioLamViec + luongThemMoiGio * soGioLamViec;
     }
-
-    public int getSoGioLamViec() {
-        return soGioLamViec;
-    }
-
-    public void setSoGioLamViec(int soGioLamViec) {
-        this.soGioLamViec = soGioLamViec;
-    }
-
-    public double getTienPhuCap() {
-        return tienPhuCap;
-    }
-
-    public void setTienPhuCap(double tienPhuCap) {
-        this.tienPhuCap = tienPhuCap;
-    }
-
     @Override
     public String toString() {
-        return super.toString() + ", soGioLamViec=" + soGioLamViec + ", tienPhuCap=" + tienPhuCap;
+        return super.toString() + ", soGioLamViec=" + soGioLamViec + ", luongCoBan=" + luongCoBan + ", luong=" + tinhLuong() + '}';
     }
 }
