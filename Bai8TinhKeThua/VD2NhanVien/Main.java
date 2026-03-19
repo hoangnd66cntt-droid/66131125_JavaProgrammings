1beafdc5153569f077f705747c60fc86d4927c19
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-19 09:28:17 +0700

    chayMain

---
+++ Bai8TinhKeThua/VD2NhanVien/Main.java
@@ -0,0 +1,10 @@
+package Bai8TinhKeThua.VD2NhanVien;
+
+public class Main {
+    public static void main(String[] args) {
+        NhanVien nv1 = new NhanVien("NV001", "Nguyen Van A", 5000);
+        System.out.println(nv1);
+        Quanly ql1 = new Quanly("QL001", "Tran Van C", 6000, 2000, 1.5);
+        System.out.println(ql1);
+    }
+}
