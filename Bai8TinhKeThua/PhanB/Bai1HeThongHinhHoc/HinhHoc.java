6672494c0749947591ea2183d308ebbc0c3b672c
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-19 08:21:58 +0700

    lopcha

---
+++ Bai8TinhKeThua/PhanB/Bai1HeThongHinhHoc/HinhHoc.java
@@ -0,0 +1,22 @@
+package Bai8TinhKeThua.PhanB.Bai1HeThongHinhHoc;
+
+public class HinhHoc {
+    private String ten;
+    private String mauSac;
+    public HinhHoc(String ten, String mauSac) {
+        this.ten = ten;
+        this.mauSac = mauSac;
+    }
+public int tinhDienTich() {
+    return dienTich;
+    }
+    public int tinhChuVi() {
+    return dienTich;
+}
+public String toString() {
+    return "HinhHoc{" +
+            "ten='" + ten + '\'' +
+            ", mauSac='" + mauSac + '\'' +
+            '}';
+}
+}
