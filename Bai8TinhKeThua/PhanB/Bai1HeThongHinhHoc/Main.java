ce2946d4b9ac8432523152b598fb08381c7a8a64
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-19 08:46:49 +0700

    XuatHinhTronVaTamgiac

--- Bai8TinhKeThua/PhanB/Bai1HeThongHinhHoc/Main.java
+++ Bai8TinhKeThua/PhanB/Bai1HeThongHinhHoc/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         HinhHoc hcn = new HinhChuNhat("Hinh chu nhat 1", "Do", 5, 3);
         System.out.println(hcn);
+        HinhHoc htg = new HinhTamGiac("Hinh tam giac 1", "Xanh", 3, 4, 5);
+        System.out.println(htg);
+        HinhHoc ht = new HinhTron("Hinh tron 1", "Vang", 4);
+        System.out.println(ht);
 }
 }
