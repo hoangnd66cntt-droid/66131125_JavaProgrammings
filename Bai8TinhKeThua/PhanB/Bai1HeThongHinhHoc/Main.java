19163c8affc26d25abaa30e87b21711afdc7db56
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-03-19 08:32:22 +0700

    Main

---
+++ Bai8TinhKeThua/PhanB/Bai1HeThongHinhHoc/Main.java
@@ -0,0 +1,8 @@
+package Bai8TinhKeThua.PhanB.Bai1HeThongHinhHoc;
+
+public class Main {
+    public static void main(String[] args) {
+        HinhHoc hcn = new HinhChuNhat("Hinh chu nhat 1", "Do", 5, 3);
+        System.out.println(hcn);
+}
+}
