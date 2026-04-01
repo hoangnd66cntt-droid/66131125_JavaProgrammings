430deb4be1c064fc30044a4b0ace7d70986fc88b
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-01 23:16:55 +0700

    Create SanPham.java

---
+++ Baitap_OOP/BT_Sp/SanPham.java
@@ -0,0 +1,27 @@
+package Baitap_OOP.BT_Sp;
+import java.util.Scanner;
+
+public class SanPham {
+    public String tenSp;
+    public double donGia;
+    public double giamGia;
+    public double getThueNhapKhau() {
+        return donGia * 0.1;
+    }
+    public void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap ten sanpham: ");
+        tenSp = scanner.nextLine();
+        System.out.print("Nhap don gia: ");
+        donGia = scanner.nextDouble();
+        System.out.print("Nhap giam gia: ");
+        giamGia = scanner.nextDouble();
+    }
+    public void xuat() {
+        System.out.println("---------------------------");
+        System.out.println("Ten sanpham: " + tenSp);
+        System.out.println("Don gia: " + String.format("%,.0f", donGia));
+        System.out.println("Giam gia: " + String.format("%,.0f", giamGia));
+        System.out.println("Thue nhap khau: " + String.format("%,.0f", getThueNhapKhau()));
+    }
+}
