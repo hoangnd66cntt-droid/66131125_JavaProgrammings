2e4d27e2225a05e6fac7d6b52815f8a7260dce72
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-01 22:42:16 +0700

    ........

--- Baitap_OOP/BT_Hocsinh/Main.java
+++ Baitap_OOP/BT_Hocsinh/Main.java
@@ -5,18 +5,56 @@ public class Main{
     public static void main(String[] args) {
 
         //bai2a
-        hocsinh hs1 = new hocsinh("Nguyen Van A", 15, "10A1");
-        hocsinh hs2 = new hocsinh("Phung Thanh D", 36, "120yenlang");
-        System.out.println(hs1);
-        System.out.println(hs2);
-        //bai2b
-     //   Scanner sc = new Scanner(System.in);
-     //   hocsinh
+        hocsinh hs1 = new hocsinh("Nguyen Van A", (short) 15, "10A1");
+        hocsinh hs2 = new hocsinh("Phung Thanh D", (short) 36, "120yenlang");
+        System.out.println(hs1.toString());
+        System.out.println(hs2.toString());
+/* 
+     //bai2b
+     Scanner sc = new Scanner(System.in);
+         hocsinh hs3=  new hocsinh();
+        System.out.print("Nhapten hoc sinh: ");
+        String tenHs3 = sc.nextLine();
+       // hs.setTenHs(tenHs3);
+        System.out.print("Nhaptuoi hoc sinh: ");
+        short tuoiHs3 = sc.nextShort();
+       // hs3.setTuoiHs((short) sc.nextInt());
+        sc.nextLine();
+        System.out.print("Nhap lop hoc sinh: ");
+         String lopHs3 = sc.nextLine();
+       // hs3.setLopHs(sc.nextLine());
+        System.out.println(hs3);
+        hs3.setTenHs(tenHs3);
+        hs3.setTuoiHs(tuoiHs3);
+        hs3.setLopHs(lopHs3);
+*/
+    //cau3a
+    System.out.print("Nhap so luong hoc sinh: ");
+    int n=sc.nextInt();
+    ArrayList<hocsinh> dsHs = new ArrayList<hocsinh>();
+    hocsinh hs= new hocsinh();
+    string tenHs;
+    short tuoiHs;
+    String lopHs;
+    for(int i=0;i<n;i++){
+        System.out.print("Nhap ten hoc sinh: ");
+        tenHs = sc.nextLine();
+        System.out.print("Nhap tuoi hoc sinh: ");
+        tuoiHs = sc.nextShort();
+         sc.nextLine();
+        System.out.print("Nhap lop hoc sinh: ");
+        lopHs = sc.nextLine();
+        hs.setTenHs(tenHs);
+        hs.setTuoiHs(tuoiHs);
+        hs.setLopHs(lopHs);
+        dsHs.add(hs);
+    }
 
 
 
 
-   
+
+
     }
 
 }
