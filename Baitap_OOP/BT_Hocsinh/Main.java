920b0ce484478c7299ef1d772a101125657ff815
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-01 22:17:51 +0700

    Create Main.java

---
+++ Baitap_OOP/BT_Hocsinh/Main.java
@@ -0,0 +1,22 @@
+package Baitap_OOP.BT_Hocsinh;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        //bai2a
+        hocsinh hs1 = new hocsinh("Nguyen Van A", 15, "10A1");
+        hocsinh hs2 = new hocsinh("Phung Thanh D", 36, "120yenlang");
+        System.out.println(hs1);
+        System.out.println(hs2);
+        //bai2b
+     //   Scanner sc = new Scanner(System.in);
+     //   hocsinh
+
+
+
+
+   
+    }
+
+}
