61b961c4d02fbec56c9745a6722c5b40db152bf5
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-08 02:37:56 +0700

    cau1

---
+++ Bai10Abstract_Interface/bai1/ThietBiDT.java
@@ -0,0 +1,16 @@
+package   Bai10Abstract_Interface.bai1;
+abstract class ThietBiDienTu {
+    String ten;
+    double gia;
+    String hangSanXuat;
+    public ThietBiDienTu(String ten, double gia, String hangSanXuat) {
+        this.ten = ten;
+        this.gia = gia;
+        this.hangSanXuat = hangSanXuat;
+    }
+    abstract void bat();
+    abstract void tat();
+    void hienThiThongTin() {
+        System.out.println("Thiết bị: " + ten + " | Hãng: " + hangSanXuat + " | Giá: " + gia + " VNĐ");
+    }
+}
