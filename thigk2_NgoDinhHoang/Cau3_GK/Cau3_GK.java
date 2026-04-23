4e2c3c312deb63671f8294a7fc421ecd5404c100
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 08:34:43 +0700

    lần thứ 2

--- thigk2_NgoDinhHoang/Cau3_GK/Cau3_GK.java
+++ thigk2_NgoDinhHoang/Cau3_GK/Cau3_GK.java
@@ -1,7 +1,12 @@
 package thigk2_NgoDinhHoang.Cau3_GK;
 import java.io.*;
+import java.util.ArrayList;
 public class Cau3_GK {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader("fi.txt"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            list.add(Integer.parseInt(line));
+        }
     }}     
