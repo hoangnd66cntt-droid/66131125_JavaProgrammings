bf96b51a9b0e9541da3584909542bb2e344f6731
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 07:54:10 +0700

    lần thứ 4

--- thigk2_NgoDinhHoang/Cau2_GK/SinhVien.java
+++ thigk2_NgoDinhHoang/Cau2_GK/SinhVien.java
@@ -5,5 +5,10 @@ public class SinhVien {
     private String hoTen;
     private String lop;
     private int namSinh;
-  
+    public SinhVien(String maSV, String hoTen, String lop, int namSinh) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.lop = lop;
+        this.namSinh = namSinh;
+    }
 }
