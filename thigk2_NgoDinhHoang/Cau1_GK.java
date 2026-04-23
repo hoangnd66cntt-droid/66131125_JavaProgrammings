e427393f138ee82358ef8312e0bffc927ccd67bb
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 07:33:56 +0700

    Lần 1

---
+++ thigk2_NgoDinhHoang/Cau1_GK.java
@@ -0,0 +1,7 @@
+package thigk2_NgoDinhHoang;
+import java.util.Scanner;
+public class Cau1_GK {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+}
