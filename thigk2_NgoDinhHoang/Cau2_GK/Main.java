0786229c1281ce78109d8df93a08a9ee44030aec
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 08:20:47 +0700

    lần thứ 9

--- thigk2_NgoDinhHoang/Cau2_GK/Main.java
+++ thigk2_NgoDinhHoang/Cau2_GK/Main.java
@@ -6,7 +6,7 @@ public class Main {
         ArrayList<SinhVien> dsSv = new ArrayList<>();
         dsSv.add(new SinhVien("66131121", "Nguyen Van A", "CNTT", 2000)); // hardcode 2 sinh vien vao danh sach
         dsSv.add(new SinhVien("66131122", "Phung Thanh D", "Kinh te", 1985));
-        dsSv.add(new SinhVien("66131123", "Hong Tham", "Dien tu", 2001));
+        dsSv.add(new SinhVien("66131123", "Hong Tham", "Dien tu", 2009));
         for (SinhVien sv : dsSv) { // duyet qua tung sinh vien trong danh sach va in thong tin
             sv.inThongTin();    
         }
@@ -24,6 +24,14 @@ public class Main {
         System.out.println("sau khi them sinh vien moi:");
         for (SinhVien sv : dsSv) { // in lai danh sach sau khi them
             sv.inThongTin();
+        }
+        System.out.println("Sinh vien co tuoi > 20:"); // ds sinh vien co tuoi >20
+        for(SinhVien sv: dsSv)
+        {
+          int tuoi = 2026 - sv.getNamSinh(); // tinh tuoi cua sinh vien
+          if(tuoi >20) { // neu tuoi > 20 thi in thong tin sinh vien do
+              sv.inThongTin();
+        }
         }
           sc.close(); // dong scanner sau khi su dung
     }
