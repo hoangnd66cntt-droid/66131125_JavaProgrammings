e61760ce7ad44d9f0dabf1b4adf6f7a6627c869d
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 07:40:27 +0700

    Lần 3

--- thigk2_NgoDinhHoang/Cau1_GK.java
+++ thigk2_NgoDinhHoang/Cau1_GK.java
@@ -8,6 +8,17 @@ public class Cau1_GK {
         System.out.print("Nhập chiều cao (m): ");
         double height = scanner.nextDouble(); // nhap chieu cao 
         double bmi = weight / (height * height); // tinh chi so BMI (can nang / (chieu cao * chieu cao))
-        
+        System.out.println("Cân nặng: " + weight + " kg");
+        System.out.println("Chiều cao: " + height + " m");
+        System.out.println("Chỉ số BMI: " + bmi);
+        if (bmi < 18.5) { //chi so bmi < 18.5 ->thieu can, tu 18.5-25 ->binh thuong, tu 25-29.9 -> thua can,>30 -> beo phi
+            System.out.println("Phân loại: Thiếu cân");
+        } else if (bmi < 25) {
+            System.out.println("Phân loại: Bình thường");
+        } else if (bmi < 30) {
+            System.out.println("Phân loại: Thừa cân");
+        } else {
+            System.out.println("Phân loại: Béo phì");
+        }
     }
 }
