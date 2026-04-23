ac55b1584fe9533bdb8e48ef345fd142eacfa789
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 07:36:24 +0700

    Update Cau1_GK.java

--- thigk2_NgoDinhHoang/Cau1_GK.java
+++ thigk2_NgoDinhHoang/Cau1_GK.java
@@ -2,6 +2,12 @@ package thigk2_NgoDinhHoang;
 import java.util.Scanner;
 public class Cau1_GK {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
- 
+        Scanner scanner = new Scanner(System.in); // tao scanner de doc du lieu tu nguoi dung
+        System.out.print("Nhập cân nặng (kg): ");
+        double weight = scanner.nextDouble(); // nhap can nang
+        System.out.print("Nhập chiều cao (m): ");
+        double height = scanner.nextDouble(); // nhap chieu cao 
+        double bmi = weight / (height * height); // tinh chi so BMI (can nang / (chieu cao * chieu cao))
+        
+    }
 }
