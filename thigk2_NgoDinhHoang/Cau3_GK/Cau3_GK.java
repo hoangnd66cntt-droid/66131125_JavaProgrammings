ceea2252daca36e0e87a4b50e3968bbcf25f8dcd
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-04-23 08:30:04 +0700

    lần 1

---
+++ thigk2_NgoDinhHoang/Cau3_GK/Cau3_GK.java
@@ -0,0 +1,7 @@
+package thigk2_NgoDinhHoang.Cau3_GK;
+import java.io.*;
+public class Cau3_GK {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("fi.txt"));
+    }}     
