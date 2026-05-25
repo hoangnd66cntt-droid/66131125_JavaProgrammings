e44086bdf15bc3d3d92a66f4e959d9a5c4bef316
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-05-26 00:06:49 +0700

    záa

--- PokerFx/PokerFx.java
+++ PokerFx/PokerFx.java
@@ -1,39 +1,70 @@
-package PokerFx;
+package HelloFx;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.geometry.Pos;
 import javafx.stage.Stage;
 
-public class PokerFx extends Application {
+import java.util.Random;
+
+public class PokerFX extends Application {
+
+    Label cards = new Label("?");
+    Label result = new Label("Nhấn nút để phát bài");
+
+    String[] rank = {
+            "2", "3", "4", "5", "6", "7",
+            "8", "9", "10", "J", "Q", "K", "A"
+    };
+
+    Random rd = new Random();
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
 
-        Label text = new Label("Poker Game");
+        Button btn = new Button();
 
-        Button btn = new Button("Phát bài");
+        btn.setText("Phát bài");
 
         btn.setOnAction(e -> {
-            text.setText("Bạn nhận được: A♠");
+
+            // random 5 lá
+            String c1 = rank[rd.nextInt(13)];
+            String c2 = rank[rd.nextInt(13)];
+            String c3 = rank[rd.nextInt(13)];
+            String c4 = rank[rd.nextInt(13)];
+            String c5 = rank[rd.nextInt(13)];
+
+            // hiện bài
+            cards.setText(c1 + "  " + c2 + "  " + c3 + "  " + c4 + "  " + c5);
+
+            // kiểm tra
+            if(c1.equals(c2) || c1.equals(c3) || c1.equals(c4) || c1.equals(c5)) {
+
+                result.setText("One Pair");
+            }
+            else {
+
+                result.setText("High Card");
+            }
         });
 
         VBox root = new VBox(20);
 
         root.setAlignment(Pos.CENTER);
 
-        root.getChildren().addAll(text, btn);
+        root.getChildren().addAll(cards, btn, result);
 
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, 500, 300);
 
-        stage.setTitle("Poker");
+        primaryStage.setTitle("Poker Game");
 
-        stage.setScene(scene);
+        primaryStage.setScene(scene);
 
-        stage.show();
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
